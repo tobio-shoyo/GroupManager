1edc180768ed8c13faa4c9861539ff6926bd13e1
ANKUSH SAHOO <92300611+tobio-shoyo@users.noreply.github.com>
2021-10-18 22:07:19 +0530

    Update truth_and_dare.py

--- haruka/modules/truth_and_dare.py
+++ haruka/modules/truth_and_dare.py
@@ -4,7 +4,6 @@ import haruka.modules.truth_and_dare_string as truth_and_dare_string
 from haruka import dispatcher
 from telegram import ParseMode, Update, Bot
 from haruka.modules.disable import DisableAbleCommandHandler
-from telegram.ext import CallbackContext, run_async
 
 @run_async
 def truth(update: Update, context: CallbackContext):
